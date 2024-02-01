e6e6442ad849077236a2891d6c2e4cfeaee716c0
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-02-01 16:27:12 +0530

    Ethers filter events (getBlock)

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -72,8 +72,11 @@ async function main(){
 
   // const receipt = await provider.getTransactionReceipt("0x5c2f1e48853d70702f199ff08fbefb229589ecc0c7ab5904d0e1583cd9feb98a")
   // console.log(receipt);
+
   // const getBlock = await provider.getBlock(19132084)  // For block tx
-  const getBlockTrx = await provider.getBlockWithTransactions(19132084)
+      // const network = await provider.getNetwork();
+      // console.log(network);
+  // const getBlockTrx = await provider.getBlockWithTransactions(19132084)
   console.log(getBlock.hash);
   // console.log(getBlockTrx);
 
@@ -82,9 +85,14 @@ async function main(){
   // const signedTransaction = "0x5c2f1e48853d70702f199ff08fbefb229589ecc0c7ab5904d0e1583cd9feb98a"
   // await provider.sendTransaction(signedTransaction)
   // console.log(signedTransaction);
+
+
+
+
+
 }
 
-main()
+// main()
 
 
 
@@ -104,3 +112,48 @@ main()
   //   // console.log(JSON.stringify(info,null , 4));
   //   console.log(info);
   // })
+
+
+// ==========Full one ................==============//
+
+// async function getPastEvents(fromBlock,toBlock) {
+//   const events = [];
+//   const provider = new ethers.providers.JsonRpcProvider("https://polygon-mumbai.infura.io/v3/96821ac4b63e4f349f5b25d47e91f571")
+
+//  for (let blockNumber = fromBlock; blockNumber <= toBlock; blockNumber++) {
+//       const block = await provider.getBlock(blockNumber);
+//       const getBlockTrx = await provider.getBlockWithTransactions(blockNumber)
+//       if(getBlockTrx.transactions.length) {
+//         getBlockTrx.transactions.forEach(async(txn)=> {
+//           const hash = txn.hash;
+          
+//           const a = await provider.getTransactionReceipt(hash);
+//           console.log(a.logs[0]);
+
+//           const sig = 'Transfer(address,address,uint256)'
+
+//           // if(a.logs.length) {
+//           //   a.logs.forEach(async(event)=> {
+//           //     const utils = ethers.utils;
+//           //     const  = keccak256(utils.toUtf8Bytes(sig));
+//           //     if(sig == )
+//           //   })}
+
+//         });
+//       }
+//   }
+// }
+
+//   const getTxn = async () => {
+//         const a =  await provider.getTransactionReceipt('0xba770b460f5e52565daf1ec3cd189d8c2d07c45fd7a5cd822d885cf4c1f2d031');
+//     console.log(a.logs[0]);
+//     const sig = 'Transfer(address,address,uint256)'
+    
+//     const utils = ethers.utils;
+//     const array = keccak256(utils.toUtf8Bytes(sig));
+//     console.log(array);
+//     const bihno = utils.defaultAbiCoder.decode(['uint256'],'0x0000000000000000000000000000000000000000000000008ac7230489e80000')[0].toString();
+//     console.log(bihno);  
+//     }
+
+//     getPastEvents(45413270,45413271);
